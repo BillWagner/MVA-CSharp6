a34e21c9084ebf08e407553c6fa702f59971c96c
Bill Wagner <bill.w.wagner@outlook.com>
2015-03-02 17:18:53 -0500

    M3.4: Make an EvilPlan ViewModel
    Just to show INotifyPropertyChanged.

---
+++ EvilGeniusUtilities/EvilGeniusUtilities/EvilPlanViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvilGeniusUtilities
+{
+    public class EvilPlanViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string EvilPlanName {
+            get { return evilPlanName;  }
+            set { evilPlanName = value; }
+        }
+        private string evilPlanName;
+    }
+}
