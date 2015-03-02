dfe5f70c1b86992f77beaa128accb976561a5faa
Bill Wagner <bill.w.wagner@outlook.com>
2015-03-02 11:12:00 -0500

    M1.3: Convert ToString to Expression Bodied Member
    Discuss the benefits for less typing, clarity what the method does.

--- EvilGeniusUtilities/EvilGeniusUtilities/EvilGenius.cs
+++ EvilGeniusUtilities/EvilGeniusUtilities/EvilGenius.cs
@@ -14,9 +14,6 @@ namespace EvilGeniusUtilities
 
         public string CatchPhrase { get; set; }
 
-        public override string ToString()
-        {
-            return Name;
-        }
+        public override string ToString() => Name;
     }
 }
